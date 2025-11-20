de7c67eb9f03aa94cd4afc8fbfbff79220e4404b
mido-io <abdelhamidfarhat@outlook.com>
2025-11-20 20:19:51 +0200

    Fix: allowing app to use express.static('dist')

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 const morgan = require('morgan')
 
 const cors = require('cors')
+
 app.use(cors())
 
-
 app.use(express.json())
+
+app.use(express.static('dist'))
 
 
 // create custom token
